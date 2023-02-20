c305ab2c0d97958897688d4305611db9ddd0720f
alezhuq <okutryk@egmail.com>
2023-02-20 21:10:27 +0200

    initial commit

---
+++ backend/rest/routes.py
@@ -0,0 +1,205 @@
+import json
+import os
+
+from flask import Blueprint, request, flash, redirect, abort
+from werkzeug.utils import secure_filename
+
+from models.models import Company, db, City, Service
+
+ALLOWED_EXTENTIONS = ('png', 'jpg', 'jpeg',)
+UPLOAD_FOLDER = 'static/uploads/'
+api = Blueprint('api', __name__)
+
+
+@api.route('/')
+def index():
+    result = {'status': 'working'}
+    return json.dumps(result)
+
+
+# @api.route('/uploads/<filename>')
+# def display_image(filename):
+#     # print('display_image filename: ' + filename)
+#     return redirect(url_for('static', filename='uploads/' + filename), code=301)
+
+
+# Helping function for upload_image
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENTIONS
+
+
+# Uploading images
+def upload_image(photo):
+    if photo.filename == '':
+        flash("No image selected")
+        return redirect(request.url)
+
+    if photo and allowed_file(photo.filename):
+        filename = secure_filename(photo.filename)
+        photo.save(os.path.join(UPLOAD_FOLDER, filename))
+
+        flash("uploaded")
+        return os.path.join(UPLOAD_FOLDER, filename)
+
+
+# Company CRUD
+@api.route("/company/<int:pk>", methods=["GET", "PUT", "DELETE"])
+def one_company(pk):
+    requested_company = Company.query.filter_by(id=pk).first()
+
+    if requested_company is None:
+        abort(404)
+
+    if request.method == "GET":
+        return [requested_company]
+
+    if request.method == "PUT":
+
+        if request.files.get("photo") is not None:
+            photo = request.files['photo']
+            filename = str(upload_image(photo))
+            requested_company.photo = filename
+        requested_company.name = request.form.get("name", requested_company.name)
+        requested_company.description = request.form.get("description", requested_company.description)
+        requested_company.website = request.form.get("website", requested_company.website)
+        requested_company.email = request.form.get("email", requested_company.email)
+        requested_company.phonenum = request.form.get("phonenum", requested_company.phonenum)
+
+        requested_company.cities = request.form.get("cities", requested_company.cities)
+
+        db.session.commit()
+
+        return [requested_company]
+
+    if request.method == "DELETE":
+        db.session.delete(requested_company)
+        db.session.commit()
+
+        return json.dumps({"result": True})
+
+
+@api.route('/company', methods=["GET", "POST"])
+def company():
+    if request.method == "GET":
+        company = Company.query.all()
+        return company
+
+    if request.method == "POST":
+        try:
+            name = request.form['name']
+            description = request.form['description']
+            website = request.form['website']
+            email = request.form['email']
+            phonenum = request.form['phonenum']
+            photo = request.files['photo']
+        except Exception:
+            return abort(404)
+
+        filename = str(upload_image(photo))
+        new_company = Company(
+            name=name,
+            description=description,
+            website=website,
+            email=email,
+            phonenum=phonenum,
+            photo=filename,
+        )
+        db.session.add(new_company)
+        db.session.commit()
+        return [new_company]
+
+
+# City CRUD
+@api.route('/city', methods=["GET", "POST"])
+def city():
+    if request.method == "GET":
+        requested_city = City.query.all()
+        return requested_city
+
+    if request.method == "POST":
+        try:
+            name = request.form['name']
+        except Exception:
+            return abort(404)
+
+        new_city = City(
+            name=name,
+        )
+        db.session.add(new_city)
+        db.session.commit()
+        return [new_city]
+
+
+@api.route("/city/<int:pk>", methods=["GET", "PUT", "DELETE"])
+def one_city(pk):
+    requested_city = City.query.filter_by(id=pk).first()
+    if requested_city is None:
+        abort(404)
+
+    if request.method == "GET":
+        return [requested_city]
+
+    if request.method == "PUT":
+        requested_city.name = request.json.get("name", requested_city.name)
+        db.session.commit()
+
+        return [requested_city]
+
+    if request.method == "DELETE":
+        db.session.delete(requested_city)
+        db.session.commit()
+
+        return json.dumps({"result": True})
+
+
+@api.route('/service', methods=['GET'])
+def get_services():
+    services = Service.query.all()
+    return services
+
+
+@api.route('/service', methods=['POST'])
+def create_service():
+    name = request.json['name']
+    description = request.json['description']
+    price = request.json['price']
+    company_id = request.json['company_id']
+    service = Service(name=name, description=description, price=price, company_id=company_id)
+    db.session.add(service)
+    db.session.commit()
+    return [service]
+
+
+@api.route('/service/<int:id>', methods=['GET'])
+def get_service(id):
+    service = Service.query.filter_by(id=id).first()
+    if service is None:
+        return {'error': 'Service not found'}, 404
+    return [service]
+
+
+@api.route('/service/<int:id>', methods=['PUT'])
+def update_service(id):
+    service = Service.query.filter_by(id=id).first()
+    if service is None:
+        return {'error': 'Service not found'}, 404
+    name = request.json.get('name', service.name)
+    description = request.json.get('description', service.description)
+    price = request.json.get('price', service.price)
+    company_id = request.json.get('company_id', service.company_id)
+    service.name = name
+    service.description = description
+    service.price = price
+    service.company_id = company_id
+    db.session.commit()
+    return [service]
+
+
+@api.route('/service/<int:id>', methods=['DELETE'])
+def delete_service(id):
+    service = Service.query.filter_by(id=id).first()
+    if service is None:
+        return {'error': 'Service not found'}, 404
+    db.session.delete(service)
+    db.session.commit()
+    return {'result': True}
---
+++ backend/tests/test_CRUD.py
@@ -0,0 +1,239 @@
+import json
+import unittest
+import io
+from PIL import Image
+from app import app, db
+from models.models import Company, Service
+
+
+class CompanyTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app_context = app.app_context()
+        self.app_context.push()
+        app.config['TESTING'] = True
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+        self.app = app.test_client()
+        with app.app_context():
+            db.create_all()
+
+        # create a test company
+        self.test_company = Company(
+            name='1Test Company new',
+            description='Test Company Description',
+            website='https://www.example.com',
+            email='test@example.com',
+            phonenum='1234567890',
+            photo='test.jpg'
+        )
+        db.session.add(self.test_company)
+        db.session.commit()
+
+    def tearDown(self):
+        with app.app_context():
+            db.session.remove()
+            db.drop_all()
+        self.app_context.pop()
+
+    def test_get_all_companies(self):
+        response = self.app.get('/company')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(self.test_company.name, str(response.data))
+
+    def test_get_one_company(self):
+        response = self.app.get('/company/1')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(self.test_company.name, str(response.data))
+
+    def test_create_company(self):
+        data = {
+            'name': '12 New Test Company newer',
+            'description': 'New Test Company Description',
+            'website': 'https://www.newexample.com',
+            'email': 'newtest@example.com',
+            'phonenum': '0987654321',
+        }
+        image = Image.new('RGB', size=(50, 50), color=(155, 0, 0))
+        image_bytes = io.BytesIO()
+        image.save(image_bytes, format='JPEG')
+        image_bytes.seek(0)
+
+        data['photo'] = (image_bytes, 'test.jpg')
+
+        response = self.app.post('/company', data=data, content_type='multipart/form-data')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(data['name'], str(response.data))
+
+    def test_update_company(self):
+        data = {
+            'name': '1Updated Test Company2',
+            'description': 'Updated Test Company Description',
+            'website': 'https://www.updatedexample.com',
+            'email': 'updatedtest@example.com',
+            'phonenum': '0987654321',
+        }
+        image = Image.new('RGB', size=(50, 50), color=(155, 0, 0))
+        image_bytes = io.BytesIO()
+        image.save(image_bytes, format='JPEG')
+        image_bytes.seek(0)
+
+        data['photo'] = (image_bytes, 'test.jpg')
+
+        response = self.app.put('/company/1', data=data, content_type='multipart/form-data')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(data['name'], str(response.data))
+
+    def test_delete_company(self):
+        response = self.app.delete('/company/1')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, b'{"result": true}')
+
+
+class ServiceTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app_context = app.app_context()
+        self.app_context.push()
+        app.config['TESTING'] = True
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+        self.app = app.test_client()
+
+        with app.app_context():
+            db.create_all()
+
+        self.test_company = Company(
+            name='1Test Company new',
+            description='Test Company Description',
+            website='https://www.example.com',
+            email='test@example.com',
+            phonenum='1234567890',
+            photo='test.jpg'
+        )
+        db.session.add(self.test_company)
+        db.session.commit()
+
+    def tearDown(self):
+        with app.app_context():
+            db.session.remove()
+            db.drop_all()
+        self.app_context.pop()
+
+    def test_create_service(self):
+        response = self.app.post('/service', json={
+            'name': 'Test Service',
+            'description': 'This is a test service',
+            'price': 100,
+            'company_id': 1
+        })
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.json), 1)
+        service = response.json[0]
+        self.assertEqual(service['name'], 'Test Service')
+        self.assertEqual(service['description'], 'This is a test service')
+        self.assertEqual(service['price'], 100)
+        self.assertEqual(service['company_id'], 1)
+
+    def test_get_service(self):
+        service = Service(name='Test Service', description='This is a test service', price=100, company_id=1)
+
+        db.session.add(service)
+        db.session.commit()
+        response = self.app.get(f'/service/{service.id}')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.json), 1)
+        service = response.json[0]
+        self.assertEqual(service['name'], 'Test Service')
+        self.assertEqual(service['description'], 'This is a test service')
+        self.assertEqual(service['price'], 100)
+        self.assertEqual(service['company_id'], 1)
+
+    def test_get_services(self):
+        service1 = Service(name='Test Service 1', description='This is a test service', price=100, company_id=1)
+        service2 = Service(name='Test Service 2', description='This is another test service', price=200, company_id=1)
+        db.session.add_all([service1, service2])
+        db.session.commit()
+        response = self.app.get('/service')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.json), 2)
+
+    def test_update_service(self):
+        service = Service(name='Test Service', description='This is a test service', price=100, company_id=1)
+
+        db.session.add(service)
+        db.session.commit()
+        response = self.app.put(f'/service/{service.id}', json={
+            'name': 'Updated Test Service',
+            'price': 200
+        })
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.json), 1)
+        service = response.json[0]
+        self.assertEqual(service['name'], 'Updated Test Service')
+        self.assertEqual(service['description'], 'This is a test service')
+        self.assertEqual(service['price'], 200)
+        self.assertEqual(service['company_id'], 1)
+
+    def test_delete_service(self):
+        service = Service(name='Test Service', description='This is a test service', price=100, company_id=1)
+
+        db.session.add(service)
+        db.session.commit()
+        response = self.app.delete(f'/service/{service.id}')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json, {'result': True})
+
+
+class CityTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app_context = app.app_context()
+        self.app_context.push()
+        app.config['TESTING'] = True
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+        self.app = app.test_client()
+        with app.app_context():
+            db.create_all()
+
+    def tearDown(self):
+        with app.app_context():
+            db.session.remove()
+            db.drop_all()
+        self.app_context.pop()
+
+    def test_create_city(self):
+        response = self.app.post('/city', data={'name': 'New York'})
+        self.assertEqual(response.status_code, 200)
+        city = json.loads(response.data)[0]
+        self.assertEqual(city['name'], 'New York')
+
+    def test_get_all_cities(self):
+        self.app.post('/city', data={'name': 'New York'})
+        self.app.post('/city', data={'name': 'San Francisco'})
+        response = self.app.get('/city')
+        self.assertEqual(response.status_code, 200)
+        cities = json.loads(response.data)
+        self.assertEqual(len(cities), 2)
+        self.assertEqual(cities[0]['name'], 'New York')
+        self.assertEqual(cities[1]['name'], 'San Francisco')
+
+    def test_get_city(self):
+        self.app.post('/city', data={'name': 'New York'})
+        response = self.app.get('/city/1')
+        self.assertEqual(response.status_code, 200)
+        city = json.loads(response.data)[0]
+        self.assertEqual(city['name'], 'New York')
+
+    def test_update_city(self):
+        self.app.post('/city', data={'name': 'New York'})
+        response = self.app.put('/city/1', json={'name': 'NYC'})
+        self.assertEqual(response.status_code, 200)
+        city = json.loads(response.data)[0]
+        self.assertEqual(city['name'], 'NYC')
+
+    def test_delete_city(self):
+        self.app.post('/city', data={'name': 'New York'})
+        response = self.app.delete('/city/1')
+        self.assertEqual(response.status_code, 200)
+        result = json.loads(response.data)
+        self.assertTrue(result['result'])
+
+
+if __name__ == "__main__":
+    unittest.main()
