4b8cb78a7429761109c68f990674771a5ab6320c
alezhuq <okutryk@egmail.com>
2023-02-21 17:14:40 +0200

    .

--- app.py
+++ app.py
@@ -21,15 +21,15 @@ app = Flask(__name__)
 
 app.config["SECRET_KEY"] = "qweasdzxcqwesadxc"
 
-if TEST is True:
-    app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///:memory:'
-else:
+if __name__ == "__main__":
     app.config["SQLALCHEMY_DATABASE_URI"] = f"""{DATABASE}://{USER}:{PASSWORD}@{HOST}:{PORT}/{NAME}"""
+
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///:memory:'
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
-app.config["MAX_CONTENT_LENGTH"] = 16*1024*1024
+app.config["MAX_CONTENT_LENGTH"] = 16 * 1024 * 1024
 
 ALLOWED_EXTENTIONS = ('png', 'jpg', 'jpeg',)
-
 
 db.init_app(app)
 migrate.init_app(app)
@@ -41,5 +41,3 @@ app.register_blueprint(api)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=8000)
-
-
