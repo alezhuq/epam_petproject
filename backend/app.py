5305774033889b29815d9b9ab09990fcd832c67a
alezhuq <okutryk@egmail.com>
2023-02-21 13:38:11 +0200

    test

--- backend/app.py
+++ backend/app.py
@@ -14,28 +14,24 @@ PASSWORD = os.getenv("PASSWORD")
 NAME = os.getenv("NAME")
 PORT = os.getenv("PORT")
 DATABASE = os.getenv("DATABASE")
-TEST = os.getenv("TEST")
+TEST = True
 UPLOAD_FOLDER = 'static/uploads/'
 
 app = Flask(__name__)
-
+ALLOWED_EXTENTIONS = ('png', 'jpg', 'jpeg',)
 app.config["SECRET_KEY"] = "qweasdzxcqwesadxc"
-
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+app.config["MAX_CONTENT_LENGTH"] = 16*1024*1024
 if TEST is True:
     app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///:memory:'
 else:
     app.config["SQLALCHEMY_DATABASE_URI"] = f"""{DATABASE}://{USER}:{PASSWORD}@{HOST}:{PORT}/{NAME}"""
-app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
-app.config["MAX_CONTENT_LENGTH"] = 16*1024*1024
 
-ALLOWED_EXTENTIONS = ('png', 'jpg', 'jpeg',)
+    db.init_app(app)
+    migrate.init_app(app)
 
-
-db.init_app(app)
-migrate.init_app(app)
-
-with app.app_context():
-    db.create_all()
+    with app.app_context():
+        db.create_all()
 
 app.register_blueprint(api)
 
